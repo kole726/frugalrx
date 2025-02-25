d1eb5dff0453b2558f08f9bcdaed54d4c6d3062e
kole726 <kole726@gmail.com>
2025-02-25 16:22:50 -0600

    reconnecting local project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['frugalrx.vercel.app'],
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'frugalrx.vercel.app',
+      },
+    ],
+  },
+  // Add these to handle caching issues
+  webpack: (config, { dev, isServer }) => {
+    // Disable caching in development
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig 
